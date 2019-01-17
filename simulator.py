0afee2802ecf7176fa1845510f0a8103757b5db8
Halid Mahama <mhalid6@icloud.com>
2019-01-17 15:46:30 +0800

    added a new line

--- simulator.py
+++ simulator.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see http://www.gnu.org/licenses/.
-#
+# Just checking out git 
 import pdb
 import os
 import sys
